45a91bb0350c77c1286bf71abe8a884c84abb6a2
bkmzpet <bkmzpett@gmail.com>
2021-04-15 16:21:40 +0500

    1.4. Вызов метода с возвращаемым типом. [#471413]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -5,10 +5,19 @@ package ru.job4j.oop;
  *  типа Cat с именем peppy. Далее мы записываем в эту
  *  переменную объект типа Cat.
  *  Теперь у нас есть две переменные peppy и sparky, которые содержат разные области памяти.
+ *  public String sound() В методе sound ключевое слово void мы заменили на тип String. Поэтому в конце метода мы должны
+ *  использовать оператор return с указанием переменной типа String.
  */
 public class Cat {
+    public String sound() {
+    String voice = "may-may";//создаем переменную String voice
+    return voice;
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();//Создали объект
         Cat sparky = new Cat();
+        String say = peppy.sound();
+        System.out.println(say);
     }
 }
