7efbe659c7573a587a26cb853f808e64dd2e82fb
bkmzpet <bkmzpett@gmail.com>
2021-04-15 15:23:45 +0500

    1.2. Вызов метода объекта [#471410]

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,28 @@
+package ru.job4j.oop;
+
+/**
+ * petya.music();Вызываем метод music(),
+ * используя переменную petya объекта класса Student.
+ *
+ */
+
+public class Student {
+
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
+    public void song(){
+        System.out.println("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();//объект student
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.song();
+        petya.song();
+        petya.song();
+    }
+}
