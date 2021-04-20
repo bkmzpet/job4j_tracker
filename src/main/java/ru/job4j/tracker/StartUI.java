4b323760c1d793dc5fad69d91344490ff9439f1d
bkmzpet <bkmzpett@gmail.com>
2021-04-20 16:02:22 +0500

    5. Tracker - хранилище [#471437]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,12 +2,15 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String currentDateTimeFormat = item.getCreated().format(formatter);
-        System.out.println(currentDateTimeFormat);
+        Tracker tracker = new Tracker();
+        tracker.add(new Item());
+        System.out.println( tracker.findById(12));
+
+
+
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,11 @@
 package ru.job4j.tracker;
+/**
+ * Метод public Item[] findByName(String key) проверяет в цикле все элементы массива items, сравнивая name
+ * (используя метод getName класса Item) с аргументом метода String key. Элементы, у которых совпадает name,
+ * копирует в результирующий массив и возвращает его. Алгоритм этого метода аналогичен методу findAll.
+ */
+
+import java.util.Arrays;
 
 public class Tracker {
     private final Item[] items = new Item[100];
@@ -9,6 +16,19 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findByName(String key) { //
+        Item[] findByName = new Item[items.length];
+        int j = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                findByName[j] = item;
+                j++;
+            }
+        }
+        return findByName = Arrays.copyOf(findByName, j);
     }
 
     public Item findById(int id) {
@@ -22,4 +42,19 @@ public class Tracker {
         }
         return rsl;
     }
+
+    public Item findAll() {
+        Item[] findAll = new Item[items.length];
+        int j = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item != null) {
+                findAll[j] = item;
+                j++;
+            }
+        }
+        return findAll[j];
+    }
 }
+
+
