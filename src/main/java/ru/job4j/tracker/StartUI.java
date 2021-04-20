a5495c5508eaa05aa99ca67421645e47f6c48c35
bkmzpet <bkmzpett@gmail.com>
2021-04-20 16:49:42 +0500

    5. Tracker - хранилище [#471437]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,8 +7,10 @@ import java.util.Scanner;
 public class StartUI {
     public static void main(String[] args) {
         Tracker tracker = new Tracker();
-        tracker.add(new Item());
-        System.out.println( tracker.findById(12));
+        Item bug = new Item("fdg");
+        tracker.add(bug);
+        System.out.println( tracker.findById(1));
+
 
 
 
