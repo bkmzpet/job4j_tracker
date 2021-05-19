1875d60f7a27f10ef10aecfecdee4bb19dd6b2d3
bkmzpet <bkmzpett@gmail.com>
2021-05-19 16:56:04 +0500

    0.4. Пользовательские исключения. [#471379]

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,29 @@
+package ru.job4j.ex;
+
+public class UserStore {
+    public static User[] findUser(User[] users, String login) throws UserNotFoundException {
+        if (users.equals(login)) {
+            throw new UserNotFoundException("Url could not be null");
+
+        }
+        return users;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (user.isValid() || (user.length() < 3)) {
+            throw new UserInvalidException("UserInvalidException");
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) throws UserNotFoundException {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        User[] user = findUser(users, "Petr Arsentev");
+        if (validate(user)) {
+            System.out.println("This user has an access");
+        }
+    }
+}
